104de48366e5b16026b75c89b40861ed26c699f3
Ruiner85 <paul.m.mackay85@gmail.com>
2020-08-27 17:43:15 +0100

    Fixing Typos

--- guess.py
+++ guess.py
@@ -2,11 +2,11 @@
 import random
 print('Hello. What is your name?')
 name = input()
-secretNumber = random.randint(1,20)
+secretNumber = random.randint(1, 20)
 print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
 
 # Ask the player to guess 6 times.
-for guessesTaken in range(1,7):
+for guessesTaken in range(1, 7):
     print('Take a guess.')
     guess = int(input())
     if guess < secretNumber:
@@ -14,7 +14,7 @@ for guessesTaken in range(1,7):
     elif guess > secretNumber:
         print('Your guess is too high')
     else:
-        break # This condition is the correct quess!
+        break # This condition is the correct guess!
 
 if guess == secretNumber:
     print('Good job, ' + name + '! You guessed my number in ' + str(guessesTaken) + ' guesses.')
